4c45101a6f1431b27507e2f765dbf81ad27592af
Wolfgang Kulhanek <wkulhane@redhat.com>
2021-02-10 13:29:23 -0500

    Updated cart-service with fixed version (and new Quarkus code)

--- cart-service/src/main/java/com/redhat/cloudnative/model/Product.java
+++ cart-service/src/main/java/com/redhat/cloudnative/model/Product.java
@@ -1,64 +1,59 @@
 package com.redhat.cloudnative.model;
 
-import io.vertx.core.json.Json;
-
-import java.util.Objects;
+import org.infinispan.protostream.annotations.ProtoFactory;
+import org.infinispan.protostream.annotations.ProtoField;
 
 public class Product {
 
-    private String itemId;
-    private String name;
-    private String desc;
-    private double price;
+  private String itemId;
+  private String name;
+  private String desc;
+  private double price;
 
-    public Product() {
+  public Product() {}
 
-    }
+  @ProtoFactory
+  public Product(String itemId, String name, String desc, double price) {
+    super();
+    this.itemId = itemId;
+    this.name = name;
+    this.desc = desc;
+    this.price = price;
+  }
 
-    public Product(String itemId, String name, String desc, double price) {
-        super();
-        this.itemId = Objects.requireNonNull(itemId);
-        this.name = Objects.requireNonNull(name);
-        this.desc = Objects.requireNonNull(desc);
-        this.price = Objects.requireNonNull(price);
-    }
+  @ProtoField(number = 1)
+  public String getItemId() {
+    return itemId;
+  }
 
-    public String getItemId() {
-        return itemId;
-    }
+  public void setItemId(String itemId) {
+    this.itemId = itemId;
+  }
 
-    public void setItemId(String itemId) {
-        this.itemId = itemId;
-    }
+  @ProtoField(number = 2)
+  public String getName() {
+    return name;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public void setName(String name) {
+    this.name = name;
+  }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+  @ProtoField(number = 3)
+  public String getDesc() {
+    return desc;
+  }
 
-    public String getDesc() {
-        return desc;
-    }
+  public void setDesc(String desc) {
+    this.desc = desc;
+  }
 
-    public void setDesc(String desc) {
-        this.desc = desc;
-    }
+  @ProtoField(number = 4, defaultValue = "0")
+  public double getPrice() {
+    return price;
+  }
 
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    @Override
-    public String toString() {
-        return Json.encode(this);
-    }
-
-
+  public void setPrice(double price) {
+    this.price = price;
+  }
 }
